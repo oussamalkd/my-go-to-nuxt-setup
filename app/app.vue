ee1fb741396a8b34b39bacaefb943c8497577ac0
oussamalkd <oussamalkd99@gmail.com>
2025-12-04 12:52:18 +0100

    chore: migrate to Nuxt 4

--- app/app.vue
+++ app/app.vue
@@ -2,10 +2,6 @@
   <div class="container mx-auto bg-green-300 h-dvh">
     <HelloWorld name="layer" />
     <BaseButton label="create" />
-    <p>{{ $t("welcome") }}</p>
-    <pre>{{ locale }}</pre>
+    <p> hello </p>
   </div>
 </template>
-<script setup lang="ts">
-const { locale, locales } = useI18n();
-</script>
